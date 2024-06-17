2553e54f187c194ba0d329b16f9942245b02ae78
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2024-06-17 19:47:44 +0200

    minor changes in addnewtask

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -1,44 +1,23 @@
-import DatePicker from "react-datepicker"
 import { useState } from "react";
-import styles from "./calendar.module.css"
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Calendar = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
 
-  
-    const [startDate, setStartDate] = useState(new Date());
-    return (
-        <DatePicker
-        wrapperClassName="customDate"
-        showIcon
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        icon={
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="0.5em"
-            height="0.5em"
-            viewBox="0 0 48 48"
-            >
-            <mask id="ipSApplication0">
-                <g fill="none" stroke="#fff" strokeLinejoin="round" strokeWidth="2">
-                <path strokeLinecap="round" d="M40.04 22v20h-32V22"></path>
-                <path
-                    fill="#fff"
-                    d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
-                ></path>
-                </g>
-            </mask>
-            <path
-                fill="currentColor"
-                d="M0 0h48v48H0z"
-                mask="url(#ipSApplication0)"
-            ></path>
-            </svg>
-        }
-        />
-    );
+  const handleSelectedDate = (date) => {
+    setSelectedDate(date);
+  };
+  return (
+    <>
+      <h4>deadline: </h4>
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleSelectedDate}
+        dateFormat="yyyy-MM-dd"
+      />
+    </>
+  );
+};
 
-}
-
-
-export default Calendar
+export default Calendar;
